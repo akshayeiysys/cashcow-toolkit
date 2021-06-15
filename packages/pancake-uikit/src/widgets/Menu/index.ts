9bdf42f312c2aeaa622a1ea03af7f34001b5e6c6
jyoti <jyotik@eiysys.in>
2021-06-15 15:31:20 +0530

    toolkit

---
+++ packages/pancake-uikit/src/widgets/Menu/index.ts
@@ -0,0 +1,5 @@
+export { default as Menu } from "./Menu";
+export { status as menuStatus, links as menuConfig } from "./config";
+export type { NavProps, Language, MenuEntry } from "./types";
+
+// change
